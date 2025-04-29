b8ef3ce1147acf739bac6d9c48c1176a04c48e6b
GitAlex0 <68864030+GitAlex0@users.noreply.github.com>
2025-04-29 17:40:01 +0200

    Add example data structure and print function for quiz questions

---
+++ test.js
@@ -0,0 +1,12 @@
+const exampleData = {
+    1: { q: "How are you", type: "mc", a: { 1: "Great", 2: "Fine", 3: "Not great", 4: "Really bad" }, s: "wellness" },
+    2: { q: "What is your favorite color?", type: "mc", a: { 1: "Red", 2: "Blue", 3: "Green", 4: "Yellow" }, s: "colour" },
+    3: { q: "What is your favorite season?", type: "mc", a: { 1: "Spring", 2: "Summer", 3: "Autumn", 4: "Winter" }, s: "season" },
+    4: { q: "How much do you agree?", type: "r", r: { min: 0, max:100 }, s: "rate" },
+};
+for (let i = 1; i < Object.keys(exampleData).length; i++){
+    printNumbers(exampleData[i]);
+}
+function printNumbers(number){
+    console.log(number);
+}
