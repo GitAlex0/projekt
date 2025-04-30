42534c9671b1eb328f7268c48bdc41a54151dae7
GitAlex0 <68864030+GitAlex0@users.noreply.github.com>
2025-04-30 17:23:12 +0200

    randomized question order

--- read.js
+++ read.js
@@ -3,6 +3,7 @@
 // Fetch the JSON data and apply it to a div
 let jsonData;
 var index = 0;
+let randomizedKeys;
 
 async function fetchData() {
     try {
@@ -20,56 +21,67 @@ async function fetchData() {
 fetchData();
 
 
-// fu();
+// umbennen, Namensbedeutung unbekannt
 function fu() {
     console.log(jsonData);
     console.log(Object.keys(jsonData).length);
     console.log('Successfully loaded data');
-    // console.log(Object.keys(jsonData));
-    // console.log(Object.keys(jsonData).sort());
+    randomizedKeys = shuffleKeys(Object.keys(jsonData));
 }
 
 
 function next() {
     clearButtons();
-    //redundant but possible useful later
-    var sortedKeys = Object.keys(jsonData).sort();
-    // console.log(sortedKeys);
-    document.getElementById("name").innerHTML = "question " + jsonData[sortedKeys[index]].q;
-    document.getElementById("age").innerHTML =  "type " + jsonData[sortedKeys[index]].type;
-    if (jsonData[sortedKeys[index]].type == "mc"){
-        document.getElementById("email").innerHTML = "answers " + Object.keys(jsonData[sortedKeys[index]].a).length;
-        for (let i = 1; i < Object.keys(jsonData[sortedKeys[index]].a).length + 1; i++){
-            createButton(jsonData[sortedKeys[index]].a[i]);
-        }
-    }else{
-        document.getElementById("slider").style.display = "flex";
-        console.log("done");
-        document.getElementById("email").innerHTML = "r";
-    }
-    // document.cookie = index+"="+jsonData[sortedKeys[index]].q+"; expires= 18 Dec 2025 UTC";
-    document.cookie = index+"="+jsonData[sortedKeys[index]].q+";";
-    // alert(document.cookie);
-    console.log(document.cookie)
-    
+    populateQuiz(jsonData[randomizedKeys[index]]);
     index++;
     console.log('Index ' + index);
     if (index >= Object.keys(jsonData).length) {
         index = 0;
+        //Sollte stattdessen zur Ergebnis Seite gehen
     }
 }
+
+function populateQuiz(question){
+    document.getElementById("name").innerHTML =   "question " + question.q;
+    document.getElementById("age").innerHTML =  "type " + question.type;
+
+    if (question.type == "mc"){
+        document.getElementById("email").innerHTML = "answers " + Object.keys(question.a).length;
+        for (let i = 1; i < Object.keys(question.a).length + 1; i++){
+            createButton(question.a[i]);
+        }
+    }else{
+        document.getElementById("slider-container").style.display = "flex";
+        console.log("done");
+        document.getElementById("email").innerHTML = "r";
+    }
+}
+
 
 function createButton(value){
     const template = document.getElementById("button-template");
     const button = template.content.firstElementChild.cloneNode(true);
     button.textContent = value;
-    document.getElementById("buttons").appendChild(button);
-    
+    document.getElementById("buttons").appendChild(button);   
 }
 
 function clearButtons(){
     const buttonContainer = document.getElementById("buttons");
     buttonContainer.innerHTML = "";
-    const sliderContainer = document.getElementById("slider");
+    const sliderContainer = document.getElementById("slider-container");
     sliderContainer.style.display = "none";
+}
+
+//nicht implementiert
+function saveCookie(){
+    document.cookie = index+"="+jsonData[sortedKeys[index]].q+";";
+    console.log(document.cookie)
+}
+
+function shuffleKeys(keys){
+    for (let i = keys.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [keys[i], keys[j]] = [keys[j], keys[i]];
+    }
+    return keys;
 }
