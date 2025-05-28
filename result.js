5440c16875ac2fb7ed2a5613988ae3b38d33be5c
GitAlex0 <68864030+GitAlex0@users.noreply.github.com>
2025-05-28 16:35:10 +0200

    Auswertung begonnen

--- result.js
+++ result.js
@@ -1,5 +1,6 @@
-let result;
+let result = "";
 let ljsonData;
+// const skillCounts = {};
 
 function loadJSONData(){
     ljsonData = JSON.parse(localStorage.getItem("jsonData"))
@@ -27,8 +28,66 @@ function resolveIds(){
             result += approval
         }
     }
-    console.log(result);
+    // console.log(result);
     document.getElementById("result").innerHTML = result;
 }
-loadJSONData()
-resolveIds()
+document.addEventListener("DOMContentLoaded", function() {
+    loadJSONData();
+    resolveIds();
+});
+
+
+function checkAnswers(){
+    ids = JSON.parse(localStorage.getItem("answers"))
+    for (let i = 0; i < Object.keys(ids).length; i++){
+        console.log(i);
+        skill = ljsonData[Object.keys(JSON.parse(localStorage.getItem("answers")))[i]].skill
+        questionType = ljsonData[Object.keys(JSON.parse(localStorage.getItem("answers")))[i]].type
+        correctAnswerId = ljsonData[Object.keys(JSON.parse(localStorage.getItem("answers")))[i]].c
+
+        //mc & c
+        if(questionType == "mc" && correctAnswerId){
+            console.log("mc-c")
+            givenAnswerId =  ids[Object.keys(ids)[i]];
+            if(givenAnswerId == correctAnswerId){
+                console.log("great job")
+                givePoints(skill, 100)
+            }else{
+                console.log("WRONG")
+                //no points
+            }
+        }
+        if(questionType == "mc" && !correctAnswerId){
+            givenAnswerId =  ids[Object.keys(ids)[i]];
+            console.log(givenAnswerId)
+            console.log("mc-nc")
+            console.log(skill)
+        }
+        if(questionType == "r"){
+            console.log("r")
+            rating = ids[Object.keys(ids)[i]]
+            givePoints(skill, rating)
+        }
+        console.log('-'.repeat(10));
+    }
+}
+
+function countQuestions(){
+    const skillCounts = {};
+    Object.values(ljsonData).forEach(entry =>{
+        if(entry.skill){
+            skillCounts[entry.skill] = (skillCounts[entry.skill] || 0) + 1;
+        }
+    })
+    return(skillCounts);
+}
+
+
+function givePoints(skill, points){
+    sC = countQuestions();
+    if(sC[skill]){
+        console.log("true" + skill);
+        rP=points/sC[skill]
+        console.log(rP)
+    }
+}
